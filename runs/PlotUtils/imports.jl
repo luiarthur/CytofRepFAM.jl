1057bc119cb29e270621c9ee3a4846f23b882a8b
Arthur Lui <luiarthur@gmail.com>
2020-02-04 16:12:47 -0800

    added plot utils

---
+++ runs/PlotUtils/imports.jl
@@ -0,0 +1,30 @@
+import Pkg
+
+path_to_env = joinpath(@__DIR__, "../../")
+Pkg.activate(path_to_env)
+
+using CytofRepFAM
+using BSON
+using Distributions
+using LaTeXStrings
+using DataFrames
+using CSV
+using DelimitedFiles
+
+include(joinpath(@__DIR__, "salso.jl"))
+include(joinpath(@__DIR__, "dden_complete.jl"))
+
+import PyCall, PyPlot
+const plt = PyPlot.plt
+PyPlot.matplotlib.use("Agg")
+
+#= Interactive plot
+PyPlot.matplotlib.use("TkAgg")
+=#
+#= Non-interactive plot 
+PyPlot.matplotlib.use("Agg")
+=#
+
+
+# TODO
+# Steal things from: Cytof5/sims/repfam_fs/test
