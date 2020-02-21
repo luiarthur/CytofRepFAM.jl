4f7d0d9dfa8b97ab030d7b507ca6e55676199e5b
Arthur Lui <luiarthur@gmail.com>
2020-02-21 14:36:20 -0800

    added comments

--- src/MCMC/ParTemp.jl
+++ src/MCMC/ParTemp.jl
@@ -3,12 +3,19 @@ Parallel Tempering
 """
 module ParTemp
 
+"""
+Compute the log probability of swapping chains of different temperatures.
+"""
 function compute_log_accept_ratio(loglike::Function, states, temps)
   s1, s2 = states
   t1, t2 = temps
   return (1/t1 - 1/t2) * (loglike(s2) - loglike(s1))
 end
 
+
+"""
+Compute the probability of swapping chains of different temperatures.
+"""
 function compute_accept_ratio(loglike::Function, states, temps)
   return exp(compute_log_accept_ratio(loglike, states, temps))
 end
