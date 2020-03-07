157b00067d45a6b325ca56d463eb317af7cf7419
Arthur Lui <luiarthur@gmail.com>
2020-03-07 15:50:49 -0800

    added cooccurence of missing data

---
+++ dev/cooccur/cooccur.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+path_to_cb_data = '../../runs/data/cb_transformed.csv'
+cb = pd.read_csv(path_to_cb_data)
+ms = [(g[1].drop(['sample_id'], axis=1).isna() * 1.0).to_numpy()
+      for g in cb.groupby('sample_id')]
+
+i = 0
+mi_counts = ms[i].T.dot(ms[i])
+plt.imshow(mi_counts / ms[i].shape[0])
+plt.colorbar()
+plt.show()
