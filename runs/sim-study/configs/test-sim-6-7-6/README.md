c0b0df99375bd457ad2a53922d1ccde056573178
Arthur Lui <luiarthur@gmail.com>
2020-03-20 11:56:29 -0700

    doing experiment with iMCMC + coarsening

---
+++ runs/sim-study/configs/test-sim-6-7-6/README.md
@@ -0,0 +1,11 @@
+# README
+
+## Experiment test simulation 6.7.6
+
+This experiment is similar to test-sim-6-7-5.
+
+However:
+- N=(2000, 2000)
+- We use Intrinsic MCMC with coarsened posteriors
+    - I hope this will be fast, mixes well, and recovers the simulation truth.
+
---
+++ runs/sim-study/configs/test-sim-6-7-6/settings.jl
@@ -0,0 +1,43 @@
+#=
+using DelimiteFiles
+=#
+
+# NOTE: These simulation setting are identical to that of test-sims-6-5 EXCEPT
+# that W_true is different. The W_true here has low abundance (< 10% each) for
+# 2 pairs (i.e. 4) features. Thus, they should be harder to recover, and
+# uncertainty in R_i should be greater.
+
+# Simulation Name
+simname = basename("$(@__DIR__)")
+println(simname); flush(stdout)
+
+# NOTE: write to scratchdir
+function outdir_suffix(pthin, batchprop, alpha)
+  return "pthin$(pthin)-batchprop$(batchprop)-alpha$(alpha)-N2000" # TODO:N2000
+end
+
+
+# NOTE: modify
+settings = let
+  Z = Matrix{Bool}(readdlm(joinpath(@__DIR__, "Z.txt"), Int, comments=true))
+  W = readdlm(joinpath(@__DIR__, "W.txt"), comments=true)
+  [Dict(:simname => simname,
+        :repfam_dist_scale => 1.0,
+        :N => [2000, 2000],
+        :Z => Z,
+        :W => W,
+        :thin_samps => 2,
+        :nburn => 1000,
+        :nsamps => 2000,
+        :Lmcmc => Dict(0 => 2, 1 => 2),
+        :Kmcmc => 10,
+        :pthin => pthin,
+        :alpha => alpha,
+        :batchprop => batchprop,
+        :dataseed => 1,
+        :mcmcseed => 1,
+        :outdir_suffix => outdir_suffix(pthin, batchprop, alpha))
+   for pthin in [2]
+   for batchprop in [.05]
+   for alpha in [10.]]
+end
