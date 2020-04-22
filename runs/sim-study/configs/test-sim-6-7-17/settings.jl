585c8cb078a57f0e96601454e1ddfbffed4ea938
Arthur Lui <luiarthur@gmail.com>
2020-04-21 18:16:16 -0700

    made a new sim with no missing data

---
+++ runs/sim-study/configs/test-sim-6-7-17/settings.jl
@@ -0,0 +1,45 @@
+#=
+using DelimiteFiles
+=#
+
+# NOTE: These simulation setting are identical to that of test-sims-6-5 EXCEPT
+# that W_true is different. The W_true here has low abundance (< 10% each) for
+# 2 pairs (i.e. 4) features. Thus, they should be harder to recover, and
+# uncertainty in R_i should be greater.
+
+# Simulation Name
+simname = basename("$(@__DIR__)")
+println(simname); flush(stdout)
+
+# NOTE: write to scratchdir
+function outdir_suffix(dataseed, mcmcseed, phi)
+  return "dataseed$(dataseed)-mcmcseed$(mcmcseed)-phi$(phi)"
+end
+
+# NOTE: modify
+settings = let
+  Z = Matrix{Bool}(readdlm(joinpath(@__DIR__, "Z.txt"), Int, comments=true))
+  W = readdlm(joinpath(@__DIR__, "W.txt"), comments=true)
+  Nfac = 2000
+  N = [1, 1] *  Nfac
+  pthin = 5
+  batchprop = 0.05
+  [Dict(:simname => simname,
+        :repfam_dist_scale => phi,
+        :N => N,
+        :Z => Z,
+        :W => W,
+        :thin_samps => 1,
+        :nburn => 3000,
+        :nsamps => 3000,
+        :Lmcmc => Dict(0 => 2, 1 => 2),
+        :Kmcmc => 10,
+        :pthin => pthin,
+        :batchprop => batchprop,
+        :dataseed => dataseed,
+        :mcmcseed => mcmcseed,
+        :outdir_suffix => outdir_suffix(dataseed, mcmcseed, phi))
+   for phi in [1.0]
+   for dataseed in 1:3
+   for mcmcseed in 1:3]
+end
