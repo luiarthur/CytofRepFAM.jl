f8c180b7d610b23760b2a3094b2c69b6aad74399
Arthur Lui <luiarthur@gmail.com>
2020-05-27 17:25:44 -0700

    making images

--- runs/sim-study/configs/test-sim-6-8-1/tsne.jl
+++ runs/sim-study/configs/test-sim-6-8-1/tsne.jl
@@ -5,7 +5,7 @@ include(joinpath(@__DIR__, "../../../PlotUtils/imports.jl"))
 using Pandas; const pd = Pandas
 
 using PyCall
-const TSNE = PyCall.pyimport("sklearn.manifold").TSNE
+TSNE = PyCall.pyimport("sklearn.manifold").TSNE
 
 function get_best_lam(samples)
   n = length(samples) 
@@ -18,40 +18,13 @@ function get_best_lam(samples)
   return [Int64.(lams[idx_best[i]][i]) for i in 1:I]
 end
 
-# sns.pairplot(x_vars=:V1, y_vars=:V2, data=df, hue=:class,
-#              plot_kws=edgecolor=Dict(:linewidth => 0, :s=> 13),
-#              aspect=1, height=5);
-# plt.savefig("test.pdf", bbox_inches="tight");
-# plt.close();
-
-# MAIN
-results_dir = "/scratchdata/alui2/cytof/results/repfam/test-sim-6-8-1"
-tsne_dir = joinpath(results_dir, "tsne")
-path_to_simdat_1 = joinpath(results_dir, "pmiss0.6-phi0.0-zind1/simdat.bson")
-path_to_simdat_2 = joinpath(results_dir, "pmiss0.6-phi0.0-zind2/simdat.bson")
-path_to_simdat_3 = joinpath(results_dir, "pmiss0.6-phi0.0-zind3/simdat.bson")
-
-phi = [0.0, 1.0, 10.0, 25.0]
-zind = [1, 2, 3]
-
-path_to_output_1 = joinpath(results_dir, "pmiss0.6-phi1.0-zind1/output.bson")
-path_to_output_2 = joinpath(results_dir, "pmiss0.6-phi1.0-zind2/output.bson")
-path_to_output_3 = joinpath(results_dir, "pmiss0.6-phi1.0-zind3/output.bson")
-
-path_to_simdat = path_to_simdat_1 
-path_to_output = path_to_output_1 
-outdir = joinpath(tsne_dir, "z1")
-use_complete_data = true
-seed = 0
-verbose = 2
-markersize = 10
-function compute_tsne(path_to_simdat, path_to_output, outdir;
-                      use_complete_data=true, seed=0, verbose=2, 
+function compute_tsne(path_to_simdat, path_to_output, imgdir;
+                      method, use_complete_data=true, seed=0, verbose=2,
                       markersize=10)
-  simdat = BSON.load(path_to_simdat_1)[:simdat]
-  output = BSON.load(path_to_output_1)
+  simdat = BSON.load(path_to_simdat)[:simdat]
+  output = BSON.load(path_to_output)
 
-  mkpath(outdir)
+  mkpath(imgdir)
 
   if use_complete_data
     y = Matrix{Float64}.(simdat[:y_complete])
@@ -77,9 +50,41 @@ function compute_tsne(path_to_simdat, path_to_output, outdir;
     sns.pairplot(x_vars=:comp1, y_vars=:comp2, data=tsne_df, hue=:rfam,
                  plot_kws=edgecolor=Dict(:linewidth => 0, :s=> markersize),
                  aspect=1, height=5);
-    plt.savefig("$(outdir)/rfam$(i).pdf", bbox_inches="tight");
+    plt.savefig("$(imgdir)/rfam$(i).pdf", bbox_inches="tight");
     plt.close();
   end
 end
 
 
+function gen_imgdir(path_to_output, suffix)
+  d = splitdir(path_to_output)[1] 
+  d1, d2 = splitdir(d)
+  return joinpath(d1, "tsne", d2, suffix)
+end
+
+get_simdat_path(path_to_output) = joinpath(splitdir(path_to_output)[1], "simdat.bson")
+
+## MAIN ###
+results_dir = "/scratchdata/alui2/cytof/results/repfam/test-sim-6-8-1"
+
+path_to_outputs = let
+  path = [joinpath(results_dir, "pmiss$(pmiss)-phi$(phi)-zind$(zind)/output.bson")
+          for phi in (0.0, 1.0),
+              zind in (1, 2, 3),
+              pmiss in (0.6)]  # pmiss in (0.0, 0.6)
+  vec(path)
+end
+
+for path_to_output in path_to_outputs
+  for method in ["rfam"]
+    path_to_simdat = get_simdat_path(path_to_output)
+    imgdir = gen_imgdir(path_to_output, method)
+    mkpath(imgdir)
+
+    println("Now on: ", imgdir)
+
+    compute_tsne(path_to_simdat, path_to_output, imgdir;
+                 use_complete_data=true, seed=0, verbose=2, 
+                 markersize=10, method=method)
+  end
+end
