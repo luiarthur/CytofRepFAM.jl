a7ae08024d563f715c8fd8f1d974144a7e58faf3
Arthur Lui <luiarthur@gmail.com>
2020-06-09 11:27:57 -0700

    setup for run3. p_i ~ Beta(1, 99)

---
+++ runs/patients/runs/run3/tex/R_plots.R
@@ -0,0 +1,29 @@
+R_plots = function(countspath, savepath=NULL) {
+  R = read.table(countspath)
+  I = NROW(R)
+  rownames(R) = paste0('S', 1:I, ' counts')
+
+  if (!is.null(savepath)) {
+    pdf(savepath)
+  }
+
+  plot.ts(t(R), xlab='number of cell subpopulations', cex=2,
+          type='b', pch=20, main='', cex.lab=1.5, cex.axis=1.3,
+           mar.multi=c(2, 5.1, 0, 2.1))
+
+  if (!is.null(savepath)) {
+    dev.off()
+  }
+}
+
+make_R_plots = function(path) {
+  countspath = paste0(path, '/img/txt/Rcounts.txt')
+  savepath = paste0(path, '/img/Rcounts.pdf')
+  R_plots(countspath, savepath)
+}
+
+for (phi in c(0, 1, 10, 25)) {
+  path = paste0('results/phi', phi)
+  make_R_plots(path)
+}
+
