cc87d2876a9db9996171798367cdd5956e3d4d7a
Arthur Lui <luiarthur@gmail.com>
2020-06-12 08:21:34 -0700

    back to W* ~ Gamma(1000, scale=0.1)

--- runs/patients/runs/run4/run.jl
+++ runs/patients/runs/run4/run.jl
@@ -104,11 +104,11 @@ end
 
     # NOTE: Use informative priors for W* and omega, to control feature
     # selection.
-    # cfs.W_star_prior = Gamma(1000, 0.1) # shape, scale. Works, but extreme?
+    cfs.W_star_prior = Gamma(1000, 0.1) # shape, scale. Works, but extreme?
     # cfs.W_star_prior = Gamma(100, 0.1) # shape, scale. Not working.
     # cfs.W_star_prior = Gamma(1/c.K, 1) # shape, scale. Not working.
     # cfs.W_star_prior = Gamma(1/c.K, c.K * 10) # shape, scale. Not working.
-    cfs.W_star_prior = Gamma(1, 100) # shape, scale. Trying.
+    # cfs.W_star_prior = Gamma(1, 100) # shape, scale. Not super helpful.
     # similar to p ~ Beta(1, 99)
     cfs.omega_prior = Normal(-5, 1)
 
