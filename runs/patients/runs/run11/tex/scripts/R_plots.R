5ab225225b7a6d2929d38deaa909b10825b4e982
Arthur Lui <luiarthur@gmail.com>
2020-07-21 16:41:52 -0700

    updated images

--- runs/patients/runs/run11/tex/scripts/R_plots.R
+++ runs/patients/runs/run11/tex/scripts/R_plots.R
@@ -40,17 +40,24 @@ Z_dist_plots = function(z1_path, z2_path, w1_path, w2_path, savepath) {
   z2_pairwise_dist = c(dist(t(Z2[, W2>0]), method='manhattan'))
   npairs = length(z1_pairwise_dist)
   J = dim(Z1)[1]
+  K = dim(Z1)[2]
 
   pdf(savepath)
   par(mfrow=c(2, 1))
   plot(table(z1_pairwise_dist)/npairs, xlim=c(0, J), 
        ylab='proportion of column pairs', xlab='Manhattan distance',
-       main='Z estimate for Sample 1')
+       main='Z estimate for Sample 1', type='b', pch=20,
+       cex.lab=1.5, cex.axis=1.3, cex=2, xaxt='n')
   # abline(v=mean(z1_pairwise_dist), lwd=3, col='red')
+  axis(1, 1:J, 1:J, las=2)
+
   plot(table(z2_pairwise_dist)/npairs, xlim=c(0, J), 
        ylab='proportion of column pairs', xlab='Manhattan distance',
-       main='Z estimate for Sample 2')
+       main='Z estimate for Sample 2', type='b', pch=20,
+       cex.lab=1.5, cex.axis=1.3, cex=2, xaxt='n')
   # abline(v=mean(z2_pairwise_dist), lwd=3, col='red')
+  axis(1, 1:J, 1:J, las=2)
+
   par(mfrow=c(1, 1))
   dev.off()
 }
