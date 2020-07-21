c5916d1468e34f476a46d58da43f49f5acb7baed
Arthur Lui <luiarthur@gmail.com>
2020-07-21 15:31:09 -0700

    add phi25, 100 in viz

--- runs/sim-study/configs/test-sim-6-8-5/tsne/viz.py
+++ runs/sim-study/configs/test-sim-6-8-5/tsne/viz.py
@@ -45,7 +45,7 @@ if __name__ == "__main__":
                     graph_tsne(tsne_df, clust, i + 1, method, outpath)
             # rfam
             method = "rfam"
-            for phi in [0, 1]:
+            for phi in [0, 1, 25, 100]:  # NOTE: mind this!
                 clust_simname = 'pmiss{}-phi{}-zind{}'.format(pmiss, phi, zind)
                 clust_path = '{}/{}-{}.csv'.format(path_to_csv, method,
                                                    clust_simname)
