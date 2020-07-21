d6210a3d561e59f6c7680d8febfe2d7c3eea3a9e
Arthur Lui <luiarthur@gmail.com>
2020-07-20 20:45:06 -0700

    submit jobs again

---
+++ runs/sim-study/configs/test-sim-6-8-5/tex/scripts/make_plots.R
@@ -0,0 +1,67 @@
+R_plots = function(countspath, savepath=NULL, xlim=NULL) {
+  R = read.table(countspath)
+  I = NROW(R)
+  rownames(R) = paste0('S', 1:I, ' counts')
+
+  if (!is.null(savepath)) {
+    pdf(savepath)
+  }
+
+  plot.ts(t(R), xlab='number of cell subpopulations', cex=2,
+          type='b', pch=20, main='', cex.lab=1.5, cex.axis=1.3,
+          mar.multi=c(2, 5.1, 0, 2.1))
+
+  if (!is.null(savepath)) {
+    dev.off()
+  }
+}
+
+make_R_plots = function(path, xlim=NULL) {
+  countspath = paste0(path, '/img/txt/Rcounts.txt')
+  savepath = paste0(path, '/img/Rcounts.pdf')
+  R_plots(countspath, savepath, xlim=xlim)
+}
+
+# NOTE: edit this.
+Z_dist_plots = function(z1_path, z2_path, w1_path, w2_path, savepath) {
+  Z1 = read.table(z1_path)
+  Z2 = read.table(z2_path)
+  W1 = read.table(w1_path)
+  W2 = read.table(w2_path)
+
+  z1_pairwise_dist = c(dist(t(Z1[, W1>0]), method='manhattan'))
+  z2_pairwise_dist = c(dist(t(Z2[, W2>0]), method='manhattan'))
+  npairs = length(z1_pairwise_dist)
+  J = dim(Z1)[1]
+
+  pdf(savepath)
+  par(mfrow=c(2, 1))
+  plot(table(z1_pairwise_dist)/npairs, xlim=c(0, J), 
+       ylab='proportion of column pairs', xlab='Manhattan distance',
+       main='Z estimate for Sample 1')
+  # abline(v=mean(z1_pairwise_dist), lwd=3, col='red')
+  plot(table(z2_pairwise_dist)/npairs, xlim=c(0, J), 
+       ylab='proportion of column pairs', xlab='Manhattan distance',
+       main='Z estimate for Sample 2')
+  # abline(v=mean(z2_pairwise_dist), lwd=3, col='red')
+  par(mfrow=c(1, 1))
+  dev.off()
+}
+
+# NOTE: edit this.
+make_Z_dist_plots = function(path) {
+  z1_path = paste0(path, '/img/txt/Z1_best.txt')
+  z2_path = paste0(path, '/img/txt/Z2_best.txt')
+  w1_path = paste0(path, '/img/txt/W1_best.txt')
+  w2_path = paste0(path, '/img/txt/W2_best.txt')
+  savepath = paste0(path, '/img/Z_dist.pdf')
+  Z_dist_plots(z1_path, z2_path, w1_path, w2_path, savepath)
+}
+
+
+### MAIN ###
+for (phi in c(0, 1, 25, 100)) {  # NOTE: mind phi
+  path = paste0('../results/phi', phi)
+  make_R_plots(path, xlim=c(10, 25))  # NOTE: mind xlim!
+  make_Z_dist_plots(path)
+}
---
+++ runs/sim-study/configs/test-sim-6-8-5/tex/scripts/make_plots.py
@@ -0,0 +1,45 @@
+# We may make a figure similar to figures 4 and 5 but with means of y_inj in
+# the cluster, instead of z_jk.  e.g., using the heatmaps in figure 6, we can
+# compute the means of y for each (feature * marker), ybar_jk, marker j and
+# feature k.  We then make a plot of ybar_jk similar to the plot in figure 4.
+# Instead of 0 or 1, we have actual expression levels at some aggregated level.
+# We can keep ordering of features according to their abundances, but we may
+# let the R heatmap function re-arrange the markers to show the difference
+# between the features (any order of the markers does not have any meaning).
+#
+# This figure can be away from having 0 or 1. Also, it can show all features
+# well.  The current heatmaps of y do not show well features with low
+# abundance. 
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sys
+
+sys.path.append('../../../../../PlotUtils')
+import blue2red
+import plot_yz
+from Population import Population
+
+if __name__ == '__main__':
+    results_path = '../results'
+
+    for phi in (0, 1, 25, 100):
+        population = Population()
+        for i in (1, 2):
+            # Read data
+            yi_path = '{}/phi{}/img/txt/y{}_mean.csv'.format(results_path, phi, i)
+            yi = np.loadtxt(yi_path, delimiter=',')
+            lami_path = '{}/phi{}/img/txt/lam{}_best.txt'.format(results_path, phi, i)
+            lami = np.loadtxt(lami_path, dtype=int)
+            wi_path = '{}/phi{}/img/txt/W{}_best.txt'.format(results_path, phi, i)
+            wi = np.loadtxt(wi_path)
+            zi_path = '{}/phi{}/img/txt/Z{}.txt'.format(results_path, phi, i)
+            zi = np.loadtxt(zi_path)
+
+            # Plot
+            plt.figure(figsize=(6,6))
+            plot_yz.plot_y_centroids(yi, lami, wi, vlim=(-3, 3), cm=blue2red.cm(6),
+                                     population=population)
+            outpath = '{}/phi{}/img/y{}_centroid.pdf'.format(results_path, phi, i)
+            plt.savefig(outpath, bbox_inches="tight")
+            plt.close()
