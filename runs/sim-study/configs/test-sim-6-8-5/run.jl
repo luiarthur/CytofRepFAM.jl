415a680542e3677755f51819aa578956759ce403
Arthur Lui <luiarthur@gmail.com>
2020-07-20 20:51:12 -0700

    added run scripts

--- runs/sim-study/configs/test-sim-6-8-5/run.jl
+++ runs/sim-study/configs/test-sim-6-8-5/run.jl
@@ -42,8 +42,10 @@ settings = let
     nsamps = 10
     nburn = 10
   else
-    nsamps = 1000 # 3000
-    nburn = 3000  # 6000
+    # nsamps = 1000
+    # nburn = 3000
+    nsamps = 3000
+    nburn = 6000
   end
 
   Dict(:Nfac => Nfac,
---
+++ runs/sim-study/configs/test-sim-6-8-5/submit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+jid1=$(sbatch submit-job-1.sh)
+sbatch --dependency=afterok:$jid1 submit-job-2.sh
