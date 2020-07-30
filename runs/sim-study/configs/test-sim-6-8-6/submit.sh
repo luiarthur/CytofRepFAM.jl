741c08c63a5edc80c8511360610f8920527e5e58
Arthur Lui <luiarthur@gmail.com>
2020-07-30 13:09:59 -0700

    greped jobid correctly

--- runs/sim-study/configs/test-sim-6-8-6/submit.sh
+++ runs/sim-study/configs/test-sim-6-8-6/submit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-jid1=$(sbatch submit-job-1.sh); echo "AAA $jid1 BBB";
-jid2=$(sbatch --dependency=afterok:$jid1 submit-job-2.sh); echo $jid2
-jid3=$(sbatch --dependency=afterok:$jid2 submit-job-3.sh); echo $jid3
+jid1=$(sbatch submit-job-1.sh | grep -oP '\d+'); echo $jid1
+jid2=$(sbatch --dependency=afterok:$jid1 submit-job-2.sh | grep -oP '\d+'); echo $jid2
+jid3=$(sbatch --dependency=afterok:$jid2 submit-job-3.sh | grep -oP '\d+'); echo $jid3
